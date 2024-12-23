f7c23312eda733970bf94255d573127f19733d26
Raditya <75125186+rad1zly@users.noreply.github.com>
2024-12-23 12:02:43 +0700

    v3.1

--- config/config.go
+++ config/config.go
@@ -1,33 +1,56 @@
+// config/config.go
 package config
 
-import (
-	"os"
-)
+import "os"
 
 type Config struct {
-	ElasticsearchURL string
-	LeakosintAPIKey  string
-	LinkedinAPIKey   string
-	TruecallerAPIKey string
-    TruecallerAPIHost  string
-}
+    // Elasticsearch Configuration
+    ElasticsearchURL      string
+    ElasticsearchUser     string
+    ElasticsearchPassword string
+
+    // Leakosint Configuration
+    LeakosintURL         string
+    LeakosintAPIKey      string
+
+    // LinkedIn Configuration
+    LinkedinURL          string
+    LinkedinAPIKey       string
+    LinkedinAPIHost      string
+
+    // Truecaller Configuration
+    TruecallerURL        string
+    TruecallerAPIKey     string
+    TruecallerAPIHost    string
 }
 
 func LoadConfig() *Config {
-	return &Config{
-		ElasticsearchURL: getEnv("ELASTICSEARCH_URL", "http://localhost:9200"),
-		LeakosintAPIKey:  getEnv("LEAKOSINT_API_KEY", ""),
-		LinkedinAPIKey:   getEnv("LINKEDIN_API_KEY", ""),
-		TruecallerAPIKey: getEnv("TRUECALLER_API_KEY", ""),
-		TruecallerAPIKey:  getEnv("TRUECALLER_API_KEY", ""),
+    return &Config{
+        // Elasticsearch Config
+        ElasticsearchURL:      getEnv("ELASTICSEARCH_URL", "http://localhost:9200"),
+        ElasticsearchUser:     getEnv("ELASTICSEARCH_USER", "elastic"),
+        ElasticsearchPassword: getEnv("ELASTICSEARCH_PASSWORD", ""),
+
+        // Leakosint Config
+        LeakosintURL:    getEnv("LEAKOSINT_URL", "https://leakosintapi.com/"),
+        LeakosintAPIKey: getEnv("LEAKOSINT_API_KEY", ""),
+
+        // LinkedIn Config
+        LinkedinURL:     getEnv("LINKEDIN_URL", "https://fresh-linkedin-profile-data.p.rapidapi.com/google-full-profiles"),
+        LinkedinAPIKey:  getEnv("LINKEDIN_API_KEY", ""),
+        LinkedinAPIHost: getEnv("LINKEDIN_API_HOST", "fresh-linkedin-profile-data.p.rapidapi.com"),
+
+        // Truecaller Config
+        TruecallerURL:     getEnv("TRUECALLER_URL", "https://truecaller-data2.p.rapidapi.com/search"),
+        TruecallerAPIKey:  getEnv("TRUECALLER_API_KEY", ""),
         TruecallerAPIHost: getEnv("TRUECALLER_API_HOST", "truecaller-data2.p.rapidapi.com"),
-	}
+    }
 }
 
 func getEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultValue
-	}
-	return value
+    value := os.Getenv(key)
+    if value == "" {
+        return defaultValue
+    }
+    return value
 }
