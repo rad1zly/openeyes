429546069db88395c86db91bd238bc548158f1f1
Raditya <75125186+rad1zly@users.noreply.github.com>
2024-12-25 12:46:33 +0700

    db

--- database/database.go
+++ database/database.go
@@ -2,13 +2,47 @@ package database
 
 import (
     "database/sql"
-    _"github.com/go-sql-driver/mysql"
+    "fmt"
+    "log"
+    "os"
+    "sync"
+
+    "github.com/joho/godotenv"
+    _ "github.com/go-sql-driver/mysql"
 )
 
-func InitDB() (*sql.DB, error) {
-    db, err := sql.Open("mysql", "username:password@tcp(localhost:3306)/database_name")
+var (
+    db   *sql.DB
+    once sync.Once
+)
+
+func initDB() {
+    err := godotenv.Load()
     if err != nil {
-        return nil, err
+        log.Fatal("Error loading .env file")
     }
-    return db, nil
+
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+    dbUser := os.Getenv("DB_USER")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
+
+    connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+    db, err = sql.Open("mysql", connectionString)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = db.Ping()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Println("Connected to the database")
+}
+
+func GetDB() *sql.DB {
+    once.Do(initDB)
+    return db
 }
