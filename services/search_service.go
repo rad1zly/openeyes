54c83483a67edc03e94355c24ba68d50f82627a7
Raditya <75125186+rad1zly@users.noreply.github.com>
2024-12-25 16:11:56 +0700

    a

--- services/search_service.go
+++ services/search_service.go
@@ -115,7 +115,7 @@ func (s *SearchService) searchLeakosint(query string) ([]models.SearchResult, er
 
     var results []models.SearchResult
     if list, ok := apiResponse["List"].(map[string]interface{}); ok {
-        for sourceKey, sourceValue := range list {
+        for _, sourceValue := range list {
             if data, ok := sourceValue.(map[string]interface{}); ok {
                 result := models.SearchResult{
                     ID:        fmt.Sprintf("leakosint_%d", time.Now().UnixNano()),
